9288595b2e2d51b4971b07594c483ea780653c5a
drvnprgrmr <ekunweibukun159357@gmail.com>
2023-06-13 18:09:24 +0100

    More styling

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+const profile = document.getElementById("profile")
+const profileNav = document.getElementById("profile-nav")
+
+const body = document.getElementById("body")
+
+body.addEventListener("click", (ev) => {
+  //TODO hide profile nav
+})
+
+
+profile.addEventListener("click", (ev) => {
+  // Toggle display of profile nav
+  
+})
