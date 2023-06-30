3049aba6f3536973a72c70aa451e35ddfea3fb1a
drvnprgrmr <ekunweibukun159357@gmail.com>
2023-06-30 10:04:41 +0100

    remove dist from git

--- dist/middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isLoggedIn(req, res, next) {
-    if (!req.session.user)
-        return res.render("notLoggedIn");
-    next();
-}
-exports.default = isLoggedIn;
